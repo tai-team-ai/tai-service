74fbc733e62d5aa77fc5132e216a5b37c20484ea
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-06-13 14:13:46 +0000

    chore: project created with projen

---
+++ .projenrc.py
@@ -0,0 +1,12 @@
+from projen.awscdk import AwsCdkPythonApp
+
+project = AwsCdkPythonApp(
+    author_email="jacobpetterle+aiforu@gmail.com",
+    author_name="Jacob Petterle",
+    cdk_version="2.1.0",
+    module_name="tai_service",
+    name="tai-service",
+    version="0.1.0",
+)
+
+project.synth()
