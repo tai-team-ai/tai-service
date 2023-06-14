c3fdf82b25710bbb34ba66e39ff5d0fe38ea3399
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-06-14 14:22:50 +0000

    add reuqirments

--- .projenrc.py
+++ .projenrc.py
@@ -3,12 +3,14 @@ from projen.awscdk import AwsCdkPythonApp
 project = AwsCdkPythonApp(
     author_email="jacobpetterle+aiforu@gmail.com",
     author_name="Jacob Petterle",
+    python = ">=3.10",
     cdk_version="2.1.0",
     module_name="tai_service",
     name="tai-service",
     version="0.1.0",
     deps=[
         "pydantic",
+        "loguru",
     ]
 )
 
