3113e2c911a8e1051e3ca6145fe190098f826ba0
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-06-17 14:14:30 +0000

    add settings to document db initializer

--- tai_service/cdk/constructs/customresources/document_db/document_db_setup_lambda.py
+++ tai_service/cdk/constructs/customresources/document_db/document_db_setup_lambda.py
@@ -6,7 +6,7 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_typing.events import CloudFormationCustomResourceEvent
 import pymongo
 from pymongo.database import Database
-from pydantic import BaseModel
+from pydantic import BaseSettings, Field
 # first imports are for local development, second imports are for deployment
 try:
     from ..custom_resource_interface import CustomResourceInterface
@@ -14,25 +14,86 @@ except ImportError:
     from custom_resource_interface import CustomResourceInterface
 
 
-SERVER_SELECTION_TIMEOUT = 30
-
 # TODO add document schema here to build indexes and shards
 DOCUMENT_MAPPING = {}
-
-# TODO add collection to document mappin
-SETTINGS = {}
-
-class CollectionSettings(BaseModel):
-    """Define the settings for the collections."""
-
-    # TODO add collection settings here
-
 
 class BuiltInMongoDBRoles(Enum):
     """Define the built-in MongoDB roles."""
 
     READ = "read"
     READ_WRITE = "readWrite"
+
+
+class BaseDocumentDBSettings(BaseSettings):
+    """Define the base settings for the document database."""
+
+    cluster_host_name: str = Field(
+        ...,
+        env="CLUSTER_HOST_NAME",
+        description="The fully qualified domain name of the cluster.",
+    )
+    cluster_port: int = Field(
+        ...,
+        env="CLUSTER_PORT",
+        description="The port number of the cluster.",
+    )
+    db_name: str = Field(
+        ...,
+        env="DB_NAME",
+        description="The name of the database.",
+    )
+    collection_names: list[str] = Field(
+        ...,
+        env="COLLECTION_NAMES",
+        description="The names of the collections in the database.",
+    )
+    server_selection_timeout: int = Field(
+        default=10,
+        const=True,
+        env="SERVER_SELECTION_TIMEOUT",
+        description="The number of seconds to wait for a server to be selected.",
+    )
+
+
+class AdminDocumentDBSettings(BaseDocumentDBSettings):
+    """Define the settings for the collections."""
+
+    admin_user_name: str = Field(
+        ...,
+        env="ADMIN_USER_NAME",
+        description="The name of the database user.",
+    )
+    admin_user_password_secret_name: str = Field(
+        ...,
+        env="ADMIN_USER_PASSWORD_SECRET_NAME",
+        description="The name of the secret containing the admin user password.",
+    )
+    read_write_user_name: str = Field(
+        default="read_write_user",
+        const=True,
+        env="READ_WRITE_USER_NAME",
+        description="The name of the database user with read/write permissions.",
+    )
+    read_write_user_password_secret_name: str = Field(
+        ...,
+        env="READ_WRITE_USER_PASSWORD_SECRET_NAME",
+        description="The name of the secret containing the read/write user password.",
+    )
+    read_only_user_name: str = Field(
+        default="read_only_user",
+        const=True,
+        env="READ_ONLY_USER_NAME",
+        description="The name of the database user with read-only permissions.",
+    )
+    read_only_user_password_secret_name: str = Field(
+        ...,
+        env="READ_ONLY_USER_PASSWORD_SECRET_NAME",
+        description="The name of the secret containing the read-only user password.",
+    )
+    collection_indexes: dict[str, list[str]] = Field(
+        ...,
+        env="COLLECTION_INDEXES",
+    )
 
 
 def lambda_handler(event: CloudFormationCustomResourceEvent, context: LambdaContext) -> None:
@@ -47,7 +108,7 @@ def lambda_handler(event: CloudFormationCustomResourceEvent, context: LambdaCont
     to include all CRUD operations.
     """
     logger.info(f"Received event: {json.dumps(event)}")
-    settings = {} # TODO add settings here
+    settings = AdminDocumentDBSettings()
     custom_resource = DocumentDBCustomResource(event, context, settings)
     custom_resource.execute_crud_operation()
 
@@ -55,26 +116,37 @@ def lambda_handler(event: CloudFormationCustomResourceEvent, context: LambdaCont
 class DocumentDBCustomResource(CustomResourceInterface):
     """Define the Lambda function for initializing the database."""
 
-    def __init__(self, event: CloudFormationCustomResourceEvent, context: LambdaContext, settings: dict) -> None:
+    def __init__(self, event: CloudFormationCustomResourceEvent, context: LambdaContext, settings: AdminDocumentDBSettings) -> None:
         super().__init__(event, context)
-        password = self.get_secret(settings["admin_credentials_secret_name"]) # TODO add secret name to settings
+        password = self.get_secret(settings.admin_user_password_secret_name)
         self._settings = settings
         self._mongo_client = self._run_operation_with_retry(self._connect_to_database, password)
-    
+
     def _connect_to_database(self, password: str) -> pymongo.MongoClient:
         logger.info("Creating MongoDB client")
+        settings = self._settings
         mongo_client = pymongo.MongoClient(
-            f"mongodb://{self._settings['user_name']}:{password}@{self._settings['host']}:{self._settings['port']}/?tls=true&retryWrites=false",
-            serverSelectionTimeoutMS=SERVER_SELECTION_TIMEOUT,
+            f"mongodb://{settings.admin_user_name}:{password}@{settings.cluster_host_name}:{settings.cluster_port}/?tls=true&retryWrites=false",
+            serverSelectionTimeoutMS=settings.server_selection_timeout,
         )
         logger.info(mongo_client.server_info())
-        logger.info("Succesfully connected to cluster.")
+        logger.info(f"Successfully connected to cluster at {settings.cluster_host_name}:{settings.cluster_port}")
         return mongo_client
 
     def _create_database(self) -> None:
-        db = self._mongo_client[SETTINGS["database_name"]] # TODO add database name to settings
-        self._run_operation_with_retry(self._create_user, db, self._settings["user_read_write_credentials_secret_name"]) # TODO add user credentials secret name to settings
-        self._run_operation_with_retry(self._create_user, db, self._settings["user_read_credentials_secret_name"]) # TODO add admin credentials secret name to settings
+        db = self._mongo_client[self._settings.db_name]
+        self._run_operation_with_retry(
+            self._create_user,
+            db,
+            self._settings.read_only_user_password_secret_name,
+            self._settings.read_only_user_name
+        )
+        self._run_operation_with_retry(
+            self._create_user,
+            db,
+            self._settings.read_write_user_password_secret_name,
+            self._settings.read_write_user_name
+        )
         self._run_operation_with_retry(self._create_shards, db, self._mongo_client)
         self._run_operation_with_retry(self._create_indexes, db)
 
@@ -86,10 +158,8 @@ class DocumentDBCustomResource(CustomResourceInterface):
     def _delete_database(self) -> None:
         raise NotImplementedError("Delete operation not implemented.")
 
-    def _create_user(self, db: Database, secret_name: str) -> None:
-        use_credentials = self.get_secret(secret_name)
-        user_name = SETTINGS["user_name_key"] # TODO add user name key to settings
-        password = use_credentials[SETTINGS["password_key"]] # TODO add password key to settings
+    def _create_user(self, db: Database, secret_name: str, user_name: str) -> None:
+        password = self.get_secret(secret_name)
         logger.info(f"Creating user: {user_name}")
         db.command({
             "createUser": user_name,
@@ -102,20 +172,16 @@ class DocumentDBCustomResource(CustomResourceInterface):
 
     def _create_shards(self, db: Database, client: pymongo.MongoClient) -> None:
         client.admin.command('enableSharding', db.name)
-        for field_name in CollectionSettings.__fields__:
-            col_name = getattr(CollectionSettings, field_name)
-            db[col_name].create_index(field_name)
+        for col_name in self._settings.collection_names:
             db.command({
                 "shardCollection": f"{db.name}.{col_name}",
-                "key": {field_name: "hashed"},
+                "key": {"_id": "hashed"},
             })
             logger.info(f"Created shard for collection: {col_name}")
 
 
     def _create_indexes(self, db: Database) -> None:
-        for field_name in CollectionSettings.__fields__:
-            col_name = getattr(CollectionSettings, field_name)
-            documents = DOCUMENT_MAPPING.get(col_name, {})
-            for doc_field_name in documents:
+        for col_name in self._settings.collection_names:
+            for doc_field_name in self._settings.collection_indexes[col_name]:
                 logger.info(f"Creating index for doc field: {doc_field_name} in collection: {col_name}")
                 db[col_name].create_index(doc_field_name)
