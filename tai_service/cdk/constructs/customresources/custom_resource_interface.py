cbbb6f66c2eaf0bba42cff40ccf88eaf2e4b8aa0
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-06-17 05:04:00 +0000

    addupdates

--- tai_service/cdk/constructs/customresources/custom_resource_interface.py
+++ tai_service/cdk/constructs/customresources/custom_resource_interface.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 import json
 from enum import Enum
 from typing import Optional
+import traceback
 from loguru import logger
 import requests
 import boto3
@@ -12,11 +13,24 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_typing.events import CloudFormationCustomResourceEvent
 
 
+DELAY_BEFORE_CONNECTION_ATTEMPT = 5
+MAX_NUM_ATTEMPTS = 3
+DELAY_BETWEEN_ATTEMPTS = 5
+
+
 class Status(Enum):
     """Define the status of the database initialization."""
 
     SUCCESS = "SUCCESS"
     FAILED = "FAILED"
+
+
+class CRUDOperation(Enum):
+    """Define the CRUD operations."""
+
+    CREATE = "Create"
+    UPDATE = "Update"
+    DELETE = "Delete"
 
 
 class CustomResourceInterface(ABC):
@@ -27,9 +41,59 @@ class CustomResourceInterface(ABC):
         self._event = event
         self._context = context
 
-    @abstractmethod
     def execute_crud_operation(self) -> None:
         """Execute the CRUD operation."""
+        request_type = self._event["RequestType"]
+        try:
+            if request_type == CRUDOperation.CREATE.value:
+                logger.info("Creating database")
+                self._create_database()
+            elif request_type == CRUDOperation.UPDATE.value:
+                logger.info("Updating database")
+                self._update_database()
+            elif request_type == CRUDOperation.DELETE.value:
+                logger.info("Deleting database")
+                self._delete_database()
+            else:
+                raise ValueError(f"Invalid request type: {request_type}")
+        except Exception as e:
+            logger.exception(e)
+            logger.exception(traceback.format_exc())
+            self._send_cloud_formation_response(
+                Status.FAILED,
+                reason=str(e),
+            )
+
+    @staticmethod
+    def get_secret(secret_name: str) -> dict:
+        """Retrieve a secret from AWS Secrets Manager."""
+        logger.info(f"Retrieving secret: {secret_name}")
+        session = boto3.session.Session()
+        boto_config = BotoConfig(
+            retries={
+                "max_attempts": MAX_NUM_ATTEMPTS,
+                "mode": "standard",
+            }
+        )
+        client = session.client(
+            service_name="secretsmanager",
+            config=boto_config,
+        )
+        secret_value_response = client.get_secret_value(SecretId=secret_name)
+        return json.loads(secret_value_response["SecretString"])
+
+    @abstractmethod
+    def _create_database(self) -> None:
+        """Create the database."""
+
+    @abstractmethod
+    def _update_database(self) -> None:
+        """Update the database."""
+
+    @abstractmethod
+    def _delete_database(self) -> None:
+        """Delete the database."""
+
 
     def _send_cloud_formation_response(
         self,
@@ -51,18 +115,14 @@ class CustomResourceInterface(ABC):
         logger.info(f"Sending response: {json.dumps(response)}")
         requests.put(self._event["ResponseURL"], data=json.dumps(response), timeout=15)
 
-    def _get_secret(secret_name: str) -> dict:
-        logger.info(f"Retrieving secret: {secret_name}")
-        session = boto3.session.Session()
-        boto_config = BotoConfig(
-            retries={
-                "max_attempts": 3,
-                "mode": "standard",
-            }
-        )
-        client = session.client(
-            service_name="secretsmanager",
-            config=boto_config,
-        )
-        secret_value_response = client.get_secret_value(SecretId=secret_name)
-        return json.loads(secret_value_response["SecretString"])
+
+    def _run_operation_with_retry(self, operation: callable, *args, **kwargs) -> None:
+        for attempt in range(MAX_NUM_ATTEMPTS):
+            try:
+                return operation(*args, **kwargs)
+            except Exception as e:
+                logger.exception(e)
+                logger.info(f"Attempt {attempt + 1} of {MAX_NUM_ATTEMPTS} failed. Retrying in {DELAY_BETWEEN_ATTEMPTS} seconds...")
+                time.sleep(DELAY_BETWEEN_ATTEMPTS)
+        logger.error(f"Failed to run operation: {operation.__name__}")
+        raise e
--- tai_service/cdk/constructs/customresources/document_db_setup_lambda.py
+++ tai_service/cdk/constructs/customresources/document_db_setup_lambda.py
@@ -11,10 +11,9 @@ from botocore.config import Config as BotoConfig
 import pymongo
 from pymongo.database import Database
 from pydantic import BaseModel
+from .custom_resource_interface import CustomResourceInterface, Status
 
-DELAY_BEFORE_CONNECTION_ATTEMPT = 5
-MAX_NUM_ATTEMPTS = 3
-DELAY_BETWEEN_ATTEMPTS = 5
+
 SERVER_SELECTION_TIMEOUT = 30
 
 # TODO add document schema here to build indexes and shards
@@ -27,12 +26,6 @@ class CollectionSettings(BaseModel):
     """Define the settings for the collections."""
 
     # TODO add collection settings here
-
-class Status(Enum):
-    """Define the status of the database initialization."""
-
-    SUCCESS = "SUCCESS"
-    FAILED = "FAILED"
 
 
 class BuiltInMongoDBRoles(Enum):
@@ -54,142 +47,92 @@ def lambda_handler(event: CloudFormationCustomResourceEvent, context: LambdaCont
     to include all CRUD operations.
     """
     logger.info(f"Received event: {json.dumps(event)}")
-    try:
-        _execute_crud_operation(event)
-        _send_cloud_formation_response(event, context, Status.SUCCESS)
-    except Exception as e:
-        logger.exception(e)
-        _send_cloud_formation_response(event, context, Status.FAILED, reason=str(e))
+    lambda_handler = DocumentDBSetupLambda(event, context)
+    lambda_handler.execute_crud_operation()
 
 
-def _execute_crud_operation(event: CloudFormationCustomResourceEvent) -> None:
-    try:
-        admin_credentials = _get_secret(SETTINGS["admin_credentials_secret_name"]) # TODO add secret name to settings
-        user_name = admin_credentials[SETTINGS["user_name_key"]] # TODO add user name key to settings
-        password = admin_credentials[SETTINGS["password_key"]] # TODO add password key to settings
-    except Exception as e:
-        logger.exception(e)
-        raise ValueError("Failed to retrieve admin credentials from Secrets Manager") from e
-    client = _get_mongo_client(user_name, password)
-    mongodb = client[SETTINGS["database_name"]] # TODO add database name to settings
-    if event["RequestType"] == "Create":
-        logger.info("Creating database")
-        _create_database(mongodb, client)
-    elif event["RequestType"] == "Update":
-        logger.info("Updating database")
-        _update_database(mongodb)
-    elif event["RequestType"] == "Delete":
-        logger.info("Deleting database")
-        _delete_database(mongodb)
-    else:
-        logger.error(f"Invalid request type: {event['RequestType']}")
-        raise ValueError(f"Invalid request type: {event['RequestType']}")
+class DocumentDBSetupLambda(CustomResourceInterface):
+    """Define the Lambda function for initializing the database."""
 
-
-def _get_mongo_client(user_name: str, password: str) -> pymongo.MongoClient:
-    logger.info("Creating MongoDB client")
-    mongo_client = pymongo.MongoClient(
-        f"mongodb://{user_name}:{password}@{SETTINGS['host']}:{SETTINGS['port']}/?tls=true&retryWrites=false",
-        serverSelectionTimeoutMS=SERVER_SELECTION_TIMEOUT,
-    )
-    logger.info(mongo_client.server_info())
-    # logger.info(f"Succesfully connected to database: {SETTINGS['database_name']}") # TODO add database name to settings
-    return mongo_client
-
-
-def _send_cloud_formation_response(
-    event: CloudFormationCustomResourceEvent,
-    context: LambdaContext,
-    response_status: Status,
-    response_data: dict=None,
-    reason: str=None
-) -> None:
-    response_body = {
-        "Status": response_status,
-        "Reason": reason or f"See the details in CloudWatch Log Stream: {context.log_stream_name}",
-        "PhysicalResourceId": context.log_stream_name,
-        "StackId": event["StackId"],
-        "RequestId": event["RequestId"],
-        "LogicalResourceId": event["LogicalResourceId"],
-        "Data": response_data,
-    }
-    logger.info(f"Response body: {json.dumps(response_body)}")
-    requests.put(event["ResponseURL"], data=json.dumps(response_body), timeout=5)
-
-
-def _get_secret(secret_name: str) -> dict:
-    logger.info(f"Retrieving secret: {secret_name}")
-    session = boto3.session.Session()
-    boto_config = BotoConfig(
-        retries={
-            "max_attempts": MAX_NUM_ATTEMPTS,
-            "mode": "standard",
-        }
-    )
-    client = session.client(
-        service_name="secretsmanager",
-        config=boto_config,
-    )
-    secret_value_response = client.get_secret_value(SecretId=secret_name)
-    return json.loads(secret_value_response["SecretString"])
-
-
-def _create_database(db: pymongo.database.Database, client: pymongo.MongoClient) -> None:
-    _run_operation_with_retry(_create_user, db, SETTINGS["user_read_write_credentials_secret_name"]) # TODO add user credentials secret name to settings
-    _run_operation_with_retry(_create_user, db, SETTINGS["user_read_credentials_secret_name"]) # TODO add admin credentials secret name to settings
-    _run_operation_with_retry(_create_shards, db, client)
-    _run_operation_with_retry(_create_indexes, db)
-
-
-def _update_database(db: Database) -> None:
-    pass
-
-
-def _delete_database(db: Database) -> None:
-    pass
-
-
-def _run_operation_with_retry(operation: callable, *args, **kwargs) -> None:
-    for attempt in range(MAX_NUM_ATTEMPTS):
+    def execute_crud_operation(self) -> None:
+        event = self._event
         try:
-            return operation(*args, **kwargs)
+            admin_credentials = self.get_secret(SETTINGS["admin_credentials_secret_name"]) # TODO add secret name to settings
+            user_name = admin_credentials[SETTINGS["user_name_key"]] # TODO add user name key to settings
+            password = admin_credentials[SETTINGS["password_key"]] # TODO add password key to settings
         except Exception as e:
             logger.exception(e)
-            logger.info(f"Attempt {attempt + 1} of {MAX_NUM_ATTEMPTS} failed. Retrying in {DELAY_BETWEEN_ATTEMPTS} seconds...")
-            time.sleep(DELAY_BETWEEN_ATTEMPTS)
-    logger.error(f"Failed to run operation: {operation.__name__}")
-    raise e
-
-def _create_user(db: Database, secret_name: str) -> None:
-    use_credentials = _get_secret(secret_name)
-    user_name = SETTINGS["user_name_key"] # TODO add user name key to settings
-    password = use_credentials[SETTINGS["password_key"]] # TODO add password key to settings
-    logger.info(f"Creating user: {user_name}")
-    db.command({
-        "createUser": user_name,
-        "pwd": password,
-        "roles": [
-            {"role": BuiltInMongoDBRoles.READ.value, "db": db.name},
-        ],
-    })
+            raise ValueError("Failed to retrieve admin credentials from Secrets Manager") from e
+        client = self._get_mongo_client(user_name, password)
+        mongodb = client[SETTINGS["database_name"]] # TODO add database name to settings
+        if event["RequestType"] == "Create":
+            logger.info("Creating database")
+            self._create_database(mongodb, client)
+        elif event["RequestType"] == "Update":
+            logger.info("Updating database")
+            self._update_database(mongodb)
+        elif event["RequestType"] == "Delete":
+            logger.info("Deleting database")
+            self._delete_database(mongodb)
+        else:
+            logger.error(f"Invalid request type: {event['RequestType']}")
+            raise ValueError(f"Invalid request type: {event['RequestType']}")
 
 
-def _create_shards(db: Database, client: pymongo.MongoClient) -> None:
-    client.admin.command('enableSharding', db.name)
-    for field_name in CollectionSettings.__fields__:
-        col_name = getattr(CollectionSettings, field_name)
-        db[col_name].create_index(field_name)
+    def _get_mongo_client(self, user_name: str, password: str) -> pymongo.MongoClient:
+        logger.info("Creating MongoDB client")
+        mongo_client = pymongo.MongoClient(
+            f"mongodb://{user_name}:{password}@{SETTINGS['host']}:{SETTINGS['port']}/?tls=true&retryWrites=false",
+            serverSelectionTimeoutMS=SERVER_SELECTION_TIMEOUT,
+        )
+        logger.info(mongo_client.server_info())
+        # logger.info(f"Succesfully connected to database: {SETTINGS['database_name']}") # TODO add database name to settings
+        return mongo_client
+
+    def _create_database(self, db: pymongo.database.Database, client: pymongo.MongoClient) -> None:
+        self._run_operation_with_retry(self._create_user, db, SETTINGS["user_read_write_credentials_secret_name"]) # TODO add user credentials secret name to settings
+        self._run_operation_with_retry(self._create_user, db, SETTINGS["user_read_credentials_secret_name"]) # TODO add admin credentials secret name to settings
+        self._run_operation_with_retry(self._create_shards, db, client)
+        self._run_operation_with_retry(self._create_indexes, db)
+
+
+    def _update_database(self, db: Database) -> None:
+        pass
+
+
+    def _delete_database(self, db: Database) -> None:
+        pass
+
+    def _create_user(self, db: Database, secret_name: str) -> None:
+        use_credentials = self.get_secret(secret_name)
+        user_name = SETTINGS["user_name_key"] # TODO add user name key to settings
+        password = use_credentials[SETTINGS["password_key"]] # TODO add password key to settings
+        logger.info(f"Creating user: {user_name}")
         db.command({
-            "shardCollection": f"{db.name}.{col_name}",
-            "key": {field_name: "hashed"},
+            "createUser": user_name,
+            "pwd": password,
+            "roles": [
+                {"role": BuiltInMongoDBRoles.READ.value, "db": db.name},
+            ],
         })
-        logger.info(f"Created shard for collection: {col_name}")
 
 
-def _create_indexes(db: Database) -> None:
-    for field_name in CollectionSettings.__fields__:
-        col_name = getattr(CollectionSettings, field_name)
-        documents = DOCUMENT_MAPPING.get(col_name, {})
-        for doc_field_name in documents:
-            logger.info(f"Creating index for doc field: {doc_field_name} in collection: {col_name}")
-            db[col_name].create_index(doc_field_name)
+    def _create_shards(self, db: Database, client: pymongo.MongoClient) -> None:
+        client.admin.command('enableSharding', db.name)
+        for field_name in CollectionSettings.__fields__:
+            col_name = getattr(CollectionSettings, field_name)
+            db[col_name].create_index(field_name)
+            db.command({
+                "shardCollection": f"{db.name}.{col_name}",
+                "key": {field_name: "hashed"},
+            })
+            logger.info(f"Created shard for collection: {col_name}")
+
+
+    def _create_indexes(self, db: Database) -> None:
+        for field_name in CollectionSettings.__fields__:
+            col_name = getattr(CollectionSettings, field_name)
+            documents = DOCUMENT_MAPPING.get(col_name, {})
+            for doc_field_name in documents:
+                logger.info(f"Creating index for doc field: {doc_field_name} in collection: {col_name}")
+                db[col_name].create_index(doc_field_name)
