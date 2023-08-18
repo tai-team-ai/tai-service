468290337ca1420d16aca68af5a5277c7c9b324a
Jacob Petterle <jacobpetterle@gmail.com>
2023-08-18 01:17:14 +0000

    add 429 response for chat, search, and summary

--- taiservice/api/routers/tai.py
+++ taiservice/api/routers/tai.py
@@ -1,6 +1,7 @@
 """Define the API endpoints for the AI responses."""
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Response, status
 try:
+    from ..taibackend.taitutors.errors import UserTokenLimitError
     from .tai_schemas import(
         SearchResults,
         ResourceSearchQuery,
@@ -10,6 +11,7 @@ try:
     from ..taibackend.backend import Backend
     from ..runtime_settings import BACKEND_ATTRIBUTE_NAME
 except ImportError:
+    from taibackend.taitutors.errors import UserTokenLimitError
     from routers.tai_schemas import (
         SearchResults,
         ResourceSearchQuery,
@@ -23,23 +25,40 @@ except ImportError:
 ROUTER = APIRouter()
 
 
+def get_error_response(response: Response, exc: Exception) -> dict:
+    """Handle exceptions."""
+    if isinstance(exc, UserTokenLimitError):
+        response.status_code = status.HTTP_429_TOO_MANY_REQUESTS
+        return {"message": "User token limit reached."}
+    return {"message": "Internal server error."}
+
+
 @ROUTER.post("/chat", response_model=ChatSessionResponse)
-def chat(chat_session: ChatSessionRequest, request: Request):
+def chat(chat_session: ChatSessionRequest, request: Request, response: Response):
     """Define the chat endpoint."""
     backend: Backend = getattr(request.app.state, BACKEND_ATTRIBUTE_NAME)
-    chat_session = backend.get_tai_response(chat_session)
-    return ChatSessionResponse.parse_obj(chat_session)
+    try:
+        chat_session = backend.get_tai_response(chat_session)
+        return ChatSessionResponse.parse_obj(chat_session)
+    except Exception as e: # pylint: disable=broad-except
+        return get_error_response(response, e)
 
 
 @ROUTER.post("/search", response_model=SearchResults)
-def search(search_query: ResourceSearchQuery, request: Request):
+def search(search_query: ResourceSearchQuery, request: Request, response: Response):
     """Define the search endpoint."""
-    backend: Backend = getattr(request.app.state, BACKEND_ATTRIBUTE_NAME)
-    return backend.search(search_query, result_type="results")
+    try:
+        backend: Backend = getattr(request.app.state, BACKEND_ATTRIBUTE_NAME)
+        return backend.search(search_query)
+    except Exception as e: # pylint: disable=broad-except
+        return get_error_response(response, e)
 
 
 @ROUTER.post("/search-summary")
-def search_summary(search_query: ResourceSearchQuery, request: Request) -> str:
+def search_summary(search_query: ResourceSearchQuery, request: Request, response: Response):
     """Define the search endpoint."""
-    backend: Backend = getattr(request.app.state, BACKEND_ATTRIBUTE_NAME)
-    return backend.search(search_query, result_type="summary")
+    try:
+        backend: Backend = getattr(request.app.state, BACKEND_ATTRIBUTE_NAME)
+        return backend.search(search_query, result_type="summary")
+    except Exception as e: # pylint: disable=broad-except
+        return get_error_response(response, e)
