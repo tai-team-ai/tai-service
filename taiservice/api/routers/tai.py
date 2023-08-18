b8d208b908079a2c3bc0513a098bf575704c48bc
Jacob Petterle <jacobpetterle@gmail.com>
2023-08-18 02:56:57 +0000

    fix issues

--- taiservice/api/routers/tai.py
+++ taiservice/api/routers/tai.py
@@ -1,36 +1,35 @@
 """Define the API endpoints for the AI responses."""
-from fastapi import APIRouter, Request, Response, status
+from fastapi import APIRouter, Request, Response, status, HTTPException
 try:
     from ..taibackend.taitutors.errors import UserTokenLimitError
+    from ..taibackend.backend import Backend
     from .tai_schemas import(
         SearchResults,
         ResourceSearchQuery,
         ChatSessionRequest,
         ChatSessionResponse,
     )
-    from ..taibackend.backend import Backend
     from ..runtime_settings import BACKEND_ATTRIBUTE_NAME
 except ImportError:
     from taibackend.taitutors.errors import UserTokenLimitError
+    from taibackend.backend import Backend
     from routers.tai_schemas import (
         SearchResults,
         ResourceSearchQuery,
         ChatSessionRequest,
         ChatSessionResponse,
     )
-    from taibackend.backend import Backend
     from runtime_settings import BACKEND_ATTRIBUTE_NAME
 
 
 ROUTER = APIRouter()
 
 
-def get_error_response(response: Response, exc: Exception) -> dict:
+def handle_error(exc: Exception) -> dict:
     """Handle exceptions."""
     if isinstance(exc, UserTokenLimitError):
-        response.status_code = status.HTTP_429_TOO_MANY_REQUESTS
-        return {"message": "User token limit reached."}
-    return {"message": "Internal server error."}
+        raise HTTPException(status_code=status.HTTP_429_TOO_MANY_REQUESTS, detail=exc.message)
+    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
 
 
 @ROUTER.post("/chat", response_model=ChatSessionResponse)
@@ -41,7 +40,7 @@ def chat(chat_session: ChatSessionRequest, request: Request, response: Response)
         chat_session = backend.get_tai_response(chat_session)
         return ChatSessionResponse.parse_obj(chat_session)
     except Exception as e: # pylint: disable=broad-except
-        return get_error_response(response, e)
+        handle_error(e)
 
 
 @ROUTER.post("/search", response_model=SearchResults)
@@ -51,7 +50,7 @@ def search(search_query: ResourceSearchQuery, request: Request, response: Respon
         backend: Backend = getattr(request.app.state, BACKEND_ATTRIBUTE_NAME)
         return backend.search(search_query)
     except Exception as e: # pylint: disable=broad-except
-        return get_error_response(response, e)
+        return handle_error(e)
 
 
 @ROUTER.post("/search-summary")
@@ -61,4 +60,4 @@ def search_summary(search_query: ResourceSearchQuery, request: Request, response
         backend: Backend = getattr(request.app.state, BACKEND_ATTRIBUTE_NAME)
         return backend.search(search_query, result_type="summary")
     except Exception as e: # pylint: disable=broad-except
-        return get_error_response(response, e)
+        return handle_error(e)
